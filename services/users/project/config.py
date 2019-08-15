6945689038c8155c65c1efb5ea7e1bf0a195894d
Kenneth <kcheung331@gmail.com>
2019-08-14 19:50:02 -0700

    1a

---
+++ services/users/project/config.py
@@ -0,0 +1,15 @@
+class BaseConfig:
+    """Base Configuration"""
+    TESTING = False
+
+class DevelopmentConfig:
+    """Development Configuration"""
+    pass
+
+class TestingConfiguration:
+    """Testing configuration"""
+    Testing = True
+
+class ProductionConfiguration:
+    """Production configuration"""
+    pass
